937f2aec1b107708eee109ad4618b41cd18c7c2a
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-01-26 22:23:17 +1100

    Winston logging example with express

--- winston.js
+++ winston.js
@@ -1,10 +1,33 @@
+const winston = require('winston');
+const expressWinston = require('express-winston');
 const express = require('express');
 const app = express();
 const port = 3001;
 
+//more options here - https://github.com/bithavoc/express-winston#request-logging
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: false,
+  msg: "HTTP {{req.method}} {{req.url}}",
+  expressFormat: true,
+  colorize: false,
+  ignoreRoute: function (req, res) { return false; }
+}));
+
 app.get('/', (req, res) => {
   res.send('Hello World! - Winston logged');
-})
+});
+
+
+app.get('/api/test', (req, res) => {
+  res.json({'message': 'Hello winston!'});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
